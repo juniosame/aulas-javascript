584a7a28cf3aa7c2d1f7c324cd63dab48307a0b0
juniosame <juniosame@hotmail.com>
2023-10-05 16:49:53 -0300

    exercicio js

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -11,6 +11,26 @@ Lista de dados indexados, sequencias acessíveis por um índice numérico.
 Lista de dados não indexados e formados por propriedades
 e valores. */
 
-// Arrays
+// Arrays (os indices sempre começam do zero)
 let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
 
+console.log(cursos); // Saída direta/estruturada (APENAS NO CONSOLE)
+console.log(cursos[2]); //SQL
+
+
+
+
+
+/*
+Exercício 
+
+1) Crie uma constante chamada "dados" e coloque nela os dados de uma pessoa: nome, sobrenome, idade, telefone e cidade.
+*/
+
+const dados = ["José", "Pereira", 25, "11 0 1234-5678", "São Paulo"]
+
+/* 
+2) Mostre no console uma mensagem contendo somente o nome e a idade da pessoa. Exemplo: "Fulano tem 25 anos."
+*/
+
+console.log(`${dados[0]} tem ${dados[2]} anos.`);
