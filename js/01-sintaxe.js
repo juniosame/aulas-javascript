32e89b6e8ef972b8f98e38f5645bd00410a79f7c
juniosame <juniosame@hotmail.com>
2023-10-05 15:29:48 -0300

    update

---
+++ js/01-sintaxe.js
@@ -0,0 +1,34 @@
+console.log("Programador Web");
+console.log("Senac Penha");
+/* Comentário - ALT+SHIFT+A */
+// Comentário - CTRL+;
+
+/* Manipulando dados */
+
+// Variáveis
+let curso = "HTML5 e CSS3"; // String/Texto
+let ano = 2023; // Int/Inteiro
+
+
+// Constantes
+const mediaMinima = 7; // Valor fixo, não se altera.
+
+// Saída de dados a partir da memória
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+
+// Saída de dados personalizados
+
+/* Estou fazendo o curso de HTML5 e CSS3 neste ano de 2023 */
+
+
+// Texto ou numeros dentro das crases (`text`) dados estaticos.
+// ${} = dados dinamicos
+
+
+// Template String/Literal: saída usando crases e ${}
+console.log(`Estou fazendo o curso de ${curso} neste ano de ${ano}.`);
+
+// Concatenação
+console.log("Estou fazendo o curso de "+curso+" neste ano de "+ano+".");
---
+++ js/02-estruturas.js
@@ -0,0 +1,16 @@
+console.log("Exemplo 2");
+
+/* Estruturas de dados 
+
+- Arrays (Vetores/Matrizes);
+Lista de dados indexados, sequencias acessíveis por um índice numérico.
+
+
+
+- Objetos
+Lista de dados não indexados e formados por propriedades
+e valores. */
+
+// Arrays
+let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
+
