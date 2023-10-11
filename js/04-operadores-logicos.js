697fe291c644f1d65ccdf2c465816f5b6ef460cb
juniosame <juniosame@hotmail.com>
2023-10-11 14:23:38 -0300

    update

---
+++ js/04-operadores-logicos.js
@@ -0,0 +1,40 @@
+/* A declaração "use strict" é usada para ativar o modo estrito (strict mode) em JavaScript. Quando ativado, o modo estrito impõe um conjunto de regras mais rigorosas para escrever código, o que ajuda a evitar erros comuns e a melhorar a qualidade do código. Algumas das características e benefícios do modo estrito incluem:
+
+1. Proibição do uso de variáveis não declaradas: No modo estrito, todas as variáveis devem ser declaradas antes de serem usadas. Isso ajuda a evitar a criação acidental de variáveis globais.
+
+2. Proibição de atribuição a propriedades de somente leitura: No modo estrito, tentar atribuir um valor a uma propriedade somente leitura lançará um erro.
+
+3. Proibição de deletar variáveis, funções ou funções nomeadas: No modo estrito, a tentativa de usar o operador "delete" em variáveis, funções ou funções nomeadas lançará um erro.
+
+4. Proibição do uso de palavras reservadas como nomes de variáveis: No modo estrito, o uso de palavras reservadas como nomes de variáveis é proibido.
+
+5. Proibição do uso do objeto global "this" em funções não estritas: No modo estrito, o valor de "this" em uma função não estrita será "undefined", em vez de ser o objeto global.
+
+Essas são apenas algumas das regras impostas pelo modo estrito. Ao usar a declaração "use strict" no início de um arquivo JavaScript ou no início de uma função, você pode aproveitar os benefícios do modo estrito e escrever um código mais seguro e confiável. */
+
+"use strict";
+
+// Requisitos da escola para aprovação
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+// Resultados do aluno
+let aluno = "Jean";
+let mediaFinal = 8.5;
+let faltas = 8;
+let resultado;
+
+console.log("---------------------------------------------");
+
+
+// Operador lógico && (AND - E)
+
+if(mediaFinal >= mediaMinima && faltas < limiteDeFaltas){
+    resultado = "Aprovado";
+} else {
+    resultado = "Reprovado";
+}
+
+console.log(`O aluno ${aluno} está ${aluno}`);
+console.log(`Média do aluno: ${mediaFinal}`);
+console.log(`Faltas do aluno: ${faltas}`);
