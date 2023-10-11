6c5b327ffaac02d2a3065c3b8ebe9433e38590c9
juniosame <juniosame@hotmail.com>
2023-10-11 16:33:49 -0300

    Update 05-loops.js

--- js/05-loops.js
+++ js/05-loops.js
@@ -16,14 +16,33 @@ Obs.: normalmente o loop é controlado através de uma variável contadora.
 //     contador++; // ++ incremento
 // }
 
+
+/* ----------------------------------------------------- */
+
+
 // EXEMPLO DO/WHILE (FAÇA/ENQAUNTO)
 
 
-let i = 1;
-do {
-    console.log(`Contador vale: ${i}`);
-    i++;
-} while(i <= 3)
+// let i = 1;
+// do {
+//     console.log(`Contador vale: ${i}`);
+//     i++;
+// } while(i <= 3)
 
 /* Obs.: normalmente variáveis de controle de repetição são chamadas de i,j ou k.
 */
+
+
+/* ----------------------------------------------------- */
+
+
+// EXEMPLO FOR (PARA)
+
+/* 
+let i = 1  // Delacaração da variavel
+i <= 10  // Condição
+i++  // Atualização
+*/
+for(let i = 1; i <= 10; i++){
+    console.log(`i vale ${i}`);
+}
