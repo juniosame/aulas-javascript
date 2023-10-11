20eaab268632d8db66fbdcd908e215d3411f8d45
juniosame <juniosame@hotmail.com>
2023-10-11 16:23:40 -0300

    update

---
+++ js/05-loops.js
@@ -0,0 +1,29 @@
+/* Comandos Repetição
+Instruções para realizar ações
+por uma quantidade específica de vezes.
+
+Comandos tradicionais:
+- While         -> ENQUANTO
+- do/while      -> FAÇA/ENQUANTO
+- for           -> PARA
+
+Obs.: normalmente o loop é controlado através de uma variável contadora.
+*/
+
+// let contador = 1;
+// while( contador <= 5){
+//     console.log(`Valor de contador: ${contador}`);
+//     contador++; // ++ incremento
+// }
+
+// EXEMPLO DO/WHILE (FAÇA/ENQAUNTO)
+
+
+let i = 1;
+do {
+    console.log(`Contador vale: ${i}`);
+    i++;
+} while(i <= 3)
+
+/* Obs.: normalmente variáveis de controle de repetição são chamadas de i,j ou k.
+*/
